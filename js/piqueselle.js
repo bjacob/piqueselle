031d78a9bb049f6aa9db249d2be4d51e49cbcbde
Alan Kligman <ack@mozilla.com>
2013-05-12 17:52:27 -0400

    Still refactoring. Some things still messy and not yet working.

--- js/piqueselle.js
+++ js/piqueselle.js
@@ -4,8 +4,10 @@
     return !!((n != 0) && ((n & (n - 1)) == 0));
   };
 
-  function Scene(context, atlas, maps) {
+  var nextSceneId = 0;
+  function Scene(context, atlas, planes) {
     var gl = context.gl;
+    var id = nextSceneId++;
 
     var vertexShaderString =
       '\n\
@@ -24,6 +26,9 @@
         dst.a = (1.0 - src.a) * dst.a; \n\
       } \n\
       \n\
+      uniform float inverseZoom; \n\
+      uniform vec3 cameraPos; \n\
+      \n\
       %GLOBALS% \n\
       \n\
       vec4 resultColor = vec4(0.0, 0.0, 0.0, 1.0); \n\
@@ -40,13 +45,15 @@
     var fragmentShaderGlobals = [atlas.fragmentShaderGlobals];
     var fragmentShaderMain = [];
 
-    maps.forEach(function(map) {
-      fragmentShaderGlobals.push(map.fragmentShaderGlobals);
-      fragmentShaderMain.push(map.fragmentShaderMain);
+    planes.forEach(function(plane) {
+      fragmentShaderGlobals.push(plane[0].fragmentShaderGlobals);
+      fragmentShaderMain.push(plane[0].fragmentShaderMain);
     });
 
-    fragmentShaderString.replace("%GLOBALS%", fragmentShaderGlobals.join(''));
-    fragmentShaderString.replace("%MAIN%", fragmentShaderMain.join(''));
+    fragmentShaderString = fragmentShaderString.replace("%GLOBALS%", fragmentShaderGlobals.join(''));
+    fragmentShaderString = fragmentShaderString.replace("%MAIN%", fragmentShaderMain.join(''));
+
+    console.log('fragment shader: ' + fragmentShaderString);
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderString);
@@ -60,28 +67,31 @@
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
 
-    var vertexPositionAttrLoc = 0;
-    gl.bindAttribLocation(program, vertexPositionAttrLoc, "vertexPosition");
+    gl.bindAttribLocation(program, context.vertexPositionAttrLoc, "vertexPosition");
 
     gl.linkProgram(program);
 
-    this.renderer = renderer;
+    // Shader uniform locations
+    var uniformLocations = {
+      'cameraPos': gl.getUniformLocation(program, "cameraPos"),
+      'inverseZoom': gl.getUniformLocation(program, "inverseZoom"),
+    };
+
     this.atlas = atlas;
-    this.maps = maps;
+    this.planes = planes;
     this.program = program;
+    this.id = id;
+    this.uniformLocations = uniformLocations;
   };
 
-  function Camera(context, position) {
-    position = position || [0.0, 0.0];
-
-    var gl = context.gl;
+  function Camera(position) {
+    this.x = position[0] || 0;
+    this.y = position[1] || 0;
   };
 
   function Renderer(context) {
     var gl = context.gl;
 
-    var vertexPositionAttrLoc = 0;
-    gl.enableVertexAttribArray(vertexPositionAttrLoc);
     var vertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
     var vertices = [ -1,  -1,
@@ -89,11 +99,50 @@
                       1,  -1,
                       1,   1 ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.vertexAttribPointer(vertexPositionAttrLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(context.vertexPositionAttrLoc, 2, gl.FLOAT, false, 0, 0);
+
+    gl.disable(gl.CULL_FACE);
 
     this.context = context;
   };
   Renderer.prototype.render = function render(scene, camera) {
+    var gl = this.context.gl;
+    var program = scene.program;
+    var nextTextureUnit = 0;
+
+    gl.useProgram(program);
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+
+    // Set camera position
+    gl.uniform3f(scene.uniformLocations['cameraPos'], camera.x, camera.y, 0);
+    gl.uniform1f(scene.uniformLocations['inverseZoom'], 1/4);
+
+    // Bind to texture units
+    var textureLocations;
+
+    textureLocations = scene.atlas.getTextureUniformLocations(program);
+    textureLocations.forEach(function(textureLocation) {
+      var textureUnit = nextTextureUnit ++;
+      var location = textureLocation[0];
+      var texture = textureLocation[1];
+      gl.activeTexture(gl.TEXTURE0 + textureUnit);
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.uniform1i(location, textureUnit);
+    });
+
+    var planes = scene.planes;
+    planes.forEach(function(plane) {
+      textureLocation = plane[0].getTextureUniformLocations(program);
+      textureLocations.forEach(function(textureLocation) {
+        var textureUnit = nextTextureUnit ++;
+        var location = textureLocation[0];
+        var texture = textureLocation[1];
+        gl.activeTexture(gl.TEXTURE0 + textureUnit);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.uniform1i(location, textureUnit);
+        gl.uniform3f(plane[0].getPositionUniformLocation(program), 0, 0, plane[1]);
+      });
+    });
   };
 
   function TextureAtlas(context, options) {
@@ -117,7 +166,6 @@
     var indexType = options['indexType'] || 'UNSIGNED_BYTE';
 
     var tileSize = options['tileSize'] || 16;
-    var fragmentShaderGlobals;
 
     var atlasTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, atlasTexture);
@@ -158,20 +206,29 @@
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
 
-    fragmentShaderGlobals =
+    var fragmentShaderGlobals =
     '\n\
       uniform sampler2D atlasSampler; \n\
       uniform sampler2D indexSampler; \n\
       const vec2 atlasSize = vec2(' + atlas['width'] + ', ' + atlas['height'] + '); \n\
       const vec2 inverseTileSize = vec2(' + 1/tileSize + ', ' + 1/tileSize + '); \n\
       const float indexSize = float(' + index['width'] + '); \n\
-      uniform float inverseZoom; \n\
-      uniform vec3 cameraPos; \n\
     \n';
 
+    this.context = context;
     this.atlasTexture = atlasTexture;
     this.indexTexture = indexTexture;
     this.fragmentShaderGlobals = fragmentShaderGlobals;
+  };
+  TextureAtlas.prototype.getTextureUniformLocations = function getTextureUniformLocations(program) {
+    var gl = this.context.gl;
+
+    var textureUniformLocations = [
+      [gl.getUniformLocation(program, 'atlasSampler'), this.atlasTexture],
+      [gl.getUniformLocation(program, 'indexSampler'), this.indexTexture]
+    ];
+
+    return textureUniformLocations;
   };
 
   var nextTilePlaneId = 0;
@@ -186,11 +243,8 @@
     var map = options['map'];
     var format = options['format'] || 'LUMINANCE';
     var type = options['type'] || 'UNSIGNED_BYTE';
-    var name = options['name'] || 'tilemap' + (nextTilePlaneId++);
+    var name = options['name'] || 'TilePlane' + (nextTilePlaneId++);
     var position = options['position'] || [0.0, 0.0, 0.0];
-
-    var fragmentShaderGlobals;
-    var fragmentShaderMain;
 
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -211,14 +265,15 @@
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-    fragmentShaderGlobals =
+    var fragmentShaderGlobals =
     '\n\
       uniform sampler2D ' + name + 'Sampler; \n\
       uniform vec3 ' + name + 'Pos; \n\
     \n';
 
-    // FIXME: cull the plane if it's behind the camera
-    fragmentShaderMain =
+
+    // FIXME: cull the plane if it's behind the camera?
+    var fragmentShaderMain =
     '\n\
       { // ' + name + '\n\
         const vec2 planeSize = vec2(' + map['width'] + ', ' + map['height'] + '); \n\
@@ -227,7 +282,7 @@
         vec2 fractionalTileInPlane = (fragPos - planePos.xy) * inverseTileSize; \n\
         vec2 tileInPlane = floor(fractionalTileInPlane); \n\
         float indexOffsetForPlane = floor(256.0 * texture2D(' + name + 'Sampler, tileInPlane / planeSize).r); \n\
-        vec2 atlasCoordForPlane = floor(256.0 * texture2D(atlasTocSampler, vec2(indexOffsetForPlane / indexSize, 0.0)).ra); \n\
+        vec2 atlasCoordForPlane = floor(256.0 * texture2D(indexSampler, vec2(indexOffsetForPlane / indexSize, 0.0)).ra); \n\
         vec2 texCoordInTile = fractionalTileInPlane - tileInPlane; \n\
         color = texture2D(atlasSampler, (atlasCoordForPlane + vec2(texCoordInTile.x, 1.0 - texCoordInTile.y)) / atlasSize); \n\
         blend(color, resultColor); \n\
@@ -238,10 +293,27 @@
       } // ' + name + '\n\
     \n';
 
+    this.context = context;
     this.texture = texture;
     this.name = name;
     this.fragmentShaderGlobals = fragmentShaderGlobals;
     this.fragmentShaderMain = fragmentShaderMain;
+  };
+  TilePlane.prototype.getTextureUniformLocations = function getTextureUniformLocations(program) {
+    var gl = this.context.gl;
+    var name = this.name;
+
+    var textureUniformLocations = [
+      [gl.getUniformLocation(program, name + 'Sampler'), this.texture]
+    ];
+
+    return textureUniformLocations;
+  };
+  TilePlane.prototype.getPositionUniformLocation = function getPositionUniformLocation(program) {
+    var gl = this.context.gl;
+    var name = this.name;
+
+    return gl.getUniformLocation(program, name + 'Pos');
   };
 
   function Context(canvasElement) {
@@ -251,6 +323,9 @@
       alpha: false
     };
     var gl = canvasElement.getContext('experimental-webgl', glOptions);
+    var vertexPositionAttrLoc = 0;
+
+    gl.enableVertexAttribArray(vertexPositionAttrLoc);
 
     this.gl = gl;
     this.Renderer = Renderer.bind(undefined, this);
@@ -258,10 +333,11 @@
     this.TextureAtlas = TextureAtlas.bind(undefined, this);
     this.TilePlane = TilePlane.bind(undefined, this);
     this.Camera = Camera.bind(undefined, this);
+    this.vertexPositionAttrLoc = vertexPositionAttrLoc;
   };
 
   window.Piqueselle = {
-    Context: Context
+    Context: Context,
   };
 
 }(window));
